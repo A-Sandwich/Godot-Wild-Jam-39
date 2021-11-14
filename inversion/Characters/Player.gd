9d21063b2954c2878ccc914b8bc2b564ba1cd183
Kyle <kbburkholder@gmail.com>
2021-11-14 16:45:30 -0500

    added tile map and you can now fall. So fun

--- inversion/Characters/Player.gd
+++ inversion/Characters/Player.gd
@@ -1,6 +1,7 @@
 extends KinematicBody2D
 
-var speed = 500 
+var speed = 400 
+var is_falling = false
 
 var velocity = Vector2.ZERO
 
@@ -8,13 +9,12 @@ var velocity = Vector2.ZERO
 func _ready():
 	$AnimatedSprite.play("down")
 
-
-
 func _physics_process(delta):
 	process_input(delta)
 	velocity = move_and_slide(velocity)
 
 func process_input(delta):
+	
 	velocity = Vector2.ZERO
 	if Input.is_action_pressed("ui_up"):
 		$AnimatedSprite.play("up")
@@ -32,4 +32,21 @@ func process_input(delta):
 		if $AnimatedSprite.flip_h:
 			$AnimatedSprite.flip_h = false
 		velocity.x += 1
-	velocity = velocity.normalized() * speed
+	velocity = apply_velocity(velocity)
+
+func apply_velocity(velocity):
+	if is_falling:
+		return apply_fall()
+	return velocity.normalized() * speed
+
+func apply_fall():
+	velocity = Vector2(0, 1)
+	velocity = velocity.normalized() * (speed * 2)
+	self.z_index = -1
+	$Camera2D.current = false
+	return velocity
+
+func _on_FallDetection_body_exited(body):
+	if get_tree().get_nodes_in_group("Grass").find(body) != -1:
+		is_falling = true
+		print("Falling")
