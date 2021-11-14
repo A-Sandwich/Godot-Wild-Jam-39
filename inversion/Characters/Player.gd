061a7ada063f53cbf05a4a605bfec764e738641d
Kyle <kbburkholder@gmail.com>
2021-11-13 21:50:49 -0500

    Added movement

--- inversion/Characters/Player.gd
+++ inversion/Characters/Player.gd
@@ -1,21 +1,35 @@
 extends KinematicBody2D
 
+var speed = 500 
+
+var velocity = Vector2.ZERO
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$AnimatedSprite.play("down")
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+
+func _physics_process(delta):
 	process_input(delta)
+	velocity = move_and_slide(velocity)
 
 func process_input(delta):
+	velocity = Vector2.ZERO
 	if Input.is_action_pressed("ui_up"):
-		pass
+		$AnimatedSprite.play("up")
+		velocity.y -= 1
 	if Input.is_action_pressed("ui_down"):
-		pass
+		$AnimatedSprite.play("down")
+		velocity.y += 1
 	if Input.is_action_pressed("ui_left"):
-		pass
-	if Input.is_action_just_pressed("ui_right"):
-		pass
+		$AnimatedSprite.play("right")
+		if not $AnimatedSprite.flip_h:
+			$AnimatedSprite.flip_h = true
+		velocity.x -= 1
+	if Input.is_action_pressed("ui_right"):
+		$AnimatedSprite.play("right")
+		if $AnimatedSprite.flip_h:
+			$AnimatedSprite.flip_h = false
+		velocity.x += 1
+	velocity = velocity.normalized() * speed
